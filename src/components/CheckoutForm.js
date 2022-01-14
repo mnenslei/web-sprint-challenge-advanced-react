582bcb00e652450f1b68868f75239d2ceeeed654
Michael Enslein <michael_enslein@michaels-mbp.myfiosgateway.com>
2022-01-14 13:43:31 -0500

    completed custom hook

--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 
+import useForm from "../hooks/useForm";
+
 const initialValue = {
   firstName: "",
   lastName: "",
@@ -14,16 +16,16 @@ const initialValue = {
 // and replace the necessary stateful logic from CheckoutForm with the hook
 
 const CheckoutForm = (props) => {
-  const [showSuccessMessage, setShowSuccessMessage] = useState(false);
-  const [values, setValues] = useState(initialValue);
+  const [showSuccessMessage, setShowSuccessMessage] = useForm(false);
+  const [values, setValues] = useForm(initialValue);
 
   const handleChanges = (e) => {
     setValues({ ...values, [e.target.name]: e.target.value });
-  };
+};
 
   const handleSubmit = (e) => {
-    e.preventDefault();
-    setShowSuccessMessage(true);
+      e.preventDefault();
+      setShowSuccessMessage(true);
   };
 
   return (
